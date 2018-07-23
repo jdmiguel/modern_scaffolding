a00977382721aeadfa8b0d93f7becdd8fd846067
Jaime de Miguel Alcobendas <Jaime.Miguel@i3television.es>
2018-07-23 09:42:29 +0200

    scaffolding created

---
+++ src/index.js
@@ -0,0 +1,5 @@
+//import css from './stylecss.css';
+require('./stylecss.css');
+require('./scss/styles.scss');
+
+console.log('Hi from index!!!');
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+            "style-loader", 
+            "css-loader"
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+            "style-loader", // creates style nodes from JS strings
+            "css-loader", // translates CSS into CommonJS
+            "sass-loader" // compiles Sass to CSS
+        ]
+      }
+    ]
+  }
+};
