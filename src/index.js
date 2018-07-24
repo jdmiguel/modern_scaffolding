6d5f6be979d0b2950ad1aac1077b8dce08198016
Jaime de Miguel Alcobendas <Jaime.Miguel@i3television.es>
2018-07-24 08:53:37 +0200

    htmlwebpack and minicsswebpack plugin added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 //import css from './stylecss.css';
-require('./stylecss.css');
+//require('./css/styles.css');
 require('./scss/styles.scss');
 
 console.log('Hi from index!!!');
--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,70 @@
 const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.js",
+  name: "webpack_config",
+
+  context: path.resolve(__dirname, "src"),
+
+  entry: "./index.js",
+
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist")
   },
+
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 9000
+  },
+
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: true 
+      }),
+      new OptimizeCSSAssetsPlugin({})
+    ]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'webpack_config',
+      template: '../assets/template-app.ejs',
+      filename: 'index.html',
+      hash: true
+    }),
+
+    new MiniCssExtractPlugin({
+      filename: "styles.css"
+    })
+  ],
+
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: [
-            "style-loader", 
-            "css-loader"
-        ]
-      },
-      {
         test: /\.scss$/,
-        use: [
+        exclude: /node_modules/,
+        /*use: [
             "style-loader", // creates style nodes from JS strings
             "css-loader", // translates CSS into CommonJS
             "sass-loader" // compiles Sass to CSS
+        ]*/
+        use: [
+          MiniCssExtractPlugin.loader,
+          //"style-loader",
+          "css-loader",
+          "sass-loader"
         ]
       }
     ]
   }
 };
+
